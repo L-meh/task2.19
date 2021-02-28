9b8bbfd6b5cd9e7d5670375d1b90017d2b1d8ddf
L-meh <79751389+L-meh@users.noreply.github.com>
2021-03-01 00:33:21 +0400

    Initial commit

---
+++ src/task2/pkg19/Task219.java
@@ -0,0 +1,16 @@
+package task2.pkg19;
+public class Task219 {
+    public static void main(String[] args) {     
+System.out.println("* * * * * * * *");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * *");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * *");
+System.out.println(" * * * * * * * *");
+System.out.println("* * * * * * * *");
+System.out.println(" * * * * * * * *");
+
+
+    }
+    
+}
